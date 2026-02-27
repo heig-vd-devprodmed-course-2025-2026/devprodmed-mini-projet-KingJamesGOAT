eb328d44d0b525bf0a67b7d53f8493b7887e7729
KingJamesGOAT <sbstevebenjamin@gmail.com>
2026-02-27 17:59:28 +0100

    Fix: Correct pluralization syntax and remove test routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Models\User;
 use App\Models\Post;
 
@@ -23,54 +22,4 @@ Route::get('/profile', function () {
         ->with(['user', 'likes'])
         ->get();
     return view('profile', ['user' => $user, 'posts' => $posts]);
-});
-
-Route::get('/test-user', function () {
-    $user = new User();
-
-    $user->first_name = 'John';
-    $user->last_name = 'Doe';
-    $user->username = 'johndoe';
-    $user->email = 'johndoe@example.com';
-
-    $user->save();
-
-    return $user;
-});
-
-Route::get('/test-post-1', function () {
-    $user = User::first();
-
-    $post = new Post();
-    $post->title = 'Mon premier post';
-    $post->content = 'Ceci est le contenu de mon premier post.';
-
-    $user->posts()->save($post);
-
-    return $post;
-});
-
-Route::get('/test-post-2', function () {
-    $user = User::find(1);
-
-    $post = new Post();
-    $post->content = 'Ceci est le contenu de mon deuxieme post.';
-
-    $post->user()->associate($user);
-
-    $post->save();
-
-    return $post;
-});
-
-Route::get('/test-like', function () {
-    /** @var User $user */
-    $user = User::find(1);
-
-    /** @var Post $post */
-    $post = Post::find(2);
-
-    $user->likes()->attach($post->id, ['reaction' => 'love']);
-
-    return $post->likes;
 });
