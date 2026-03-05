85b02c38eda9490422f865298123c3394ed02a38
KingJamesGOAT <sbstevebenjamin@gmail.com>
2026-03-05 14:06:04 +0100

    test: Ajout de tests de navigation avec Pest

--- tests/Feature/NavigationTest.php
+++ tests/Feature/NavigationTest.php
@@ -3,38 +3,39 @@
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use function Pest\Laravel\get;
 
 uses(RefreshDatabase::class);
 
 it('charge la page d accueil avec succes', function () {
-    $response = $this->get('/');
+    $response = get('/');
     $response->assertStatus(200);
 });
 
 it('charge la page a propos avec succes', function () {
-    $response = $this->get('/about');
+    $response = get('/about');
     $response->assertStatus(200);
 });
 
 it('charge un profil utilisateur existant', function () {
-    $user = User::forceCreate(['first_name' => 'Test', 'last_name' => 'User', 'username' => 'testuser', 'email' => 'test@test.com']);
-    $response = $this->get('/@' . $user->username);
+    $user = User::forceCreate(['first_name' => 'Test', 'last_name' => 'User', 'username' => 'testuser1', 'email' => 'test1@test.com']);
+    $response = get('/@' . $user->username);
     $response->assertStatus(200);
 });
 
 it('charge un post existant', function () {
-    $user = User::forceCreate(['first_name' => 'Test', 'last_name' => 'User', 'username' => 'testuser', 'email' => 'test@test.com']);
+    $user = User::forceCreate(['first_name' => 'Test', 'last_name' => 'User', 'username' => 'testuser2', 'email' => 'test2@test.com']);
     $post = Post::forceCreate(['title' => 'Test', 'content' => 'Test', 'user_id' => $user->id]);
-    $response = $this->get('/posts/' . $post->id);
+    $response = get('/posts/' . $post->id);
     $response->assertStatus(200);
 });
 
 it('renvoie une erreur 404 pour un utilisateur introuvable', function () {
-    $response = $this->get('/@utilisateurfantome');
+    $response = get('/@utilisateurfantome');
     $response->assertStatus(404);
 });
 
 it('renvoie une erreur 404 pour un post introuvable', function () {
-    $response = $this->get('/posts/999999999');
+    $response = get('/posts/999999999');
     $response->assertStatus(404);
 });
